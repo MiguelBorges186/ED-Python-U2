055d90f3eb064f61897a7c98cda9e137be83e936
Miguel Borges <miguelangelborgesborges@gmail.com>
2025-09-29 08:28:07 -0600

    Add files via upload

---
+++ pilas.py
@@ -0,0 +1,15 @@
+pila = []       
+
+
+pila.append("A")
+pila.append("B")
+pila.append("C")
+print("Pila:", pila)
+
+
+x = pila.pop()
+print("Se quitó:", x)
+print("Pila ahora:", pila)
+
+
+print("Tope actual:", pila[-1]) 
